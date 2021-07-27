73499c3df895ffb18e68bf6a0f611b9d86e943d3
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-28 00:09:44 +0300

    define constructor and add the datatypes of each value

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,13 @@
 export class Repo {
+  constructor(
+    public name : string,
+    public html_url : string,
+    public updated_at : Date,
+    public created_at : Date,
+    public forks : number,
+    public language : string,
+    public clone_url : string,
+    public homepage : string,
+    public description: string,
+  ){} 
 }
