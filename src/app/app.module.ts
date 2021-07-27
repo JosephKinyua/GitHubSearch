cb92781378d9f5c8213f45d4bab90e29c917aa3b
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-27 23:59:39 +0300

    add a components search query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ReposearchComponent } from './reposearch/reposearch.component';
+import { SearchqueryComponent } from './searchquery/searchquery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ReposearchComponent
+    ReposearchComponent,
+    SearchqueryComponent
   ],
   imports: [
     BrowserModule,
