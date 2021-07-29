c2755275a68acc6616d4d389f1ca214be66c7c92
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-29 07:25:10 +0300

    import classes

--- src/app/home.service.ts
+++ src/app/home.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
 
+import { Userdetails } from './userdetails';
+import { Repo } from './repo';
+import { Searchrepo } from './searchrepo';
+
 @Injectable({
   providedIn: 'root'
 })
