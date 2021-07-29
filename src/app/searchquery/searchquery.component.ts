06ee7fd39aafd794d1a585b58968f60e11c7e58b
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-29 12:00:56 +0300

    importing the Github services

--- src/app/searchquery/searchquery.component.ts
+++ src/app/searchquery/searchquery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GithubService } from '../github.service';
 @Component({
   selector: 'app-searchquery',
   templateUrl: './searchquery.component.html',
