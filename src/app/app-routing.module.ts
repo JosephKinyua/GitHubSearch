242f888a17d4b8e7ddddd68056672df2aa0c762f
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-29 12:36:59 +0300

    boostraping the element by downloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RepoSearchComponent } from './repo-search/repo-search.component';
-import { SearchQueryComponent } from './search-query/search-query.component';
+import { ReposearchComponent } from './reposearch/reposearch.component';
+import { SearchqueryComponent } from './searchquery/searchquery.component';
 
 
 const routes: Routes = [
@@ -10,10 +10,10 @@ const routes: Routes = [
     path: 'index', component: HomeComponent
   },
   {
-    path: 'home', component:SearchQueryComponent
+    path: 'home', component:SearchqueryComponent
   },
   {
-    path: 'repos', component:RepoSearchComponent
+    path: 'repos', component:ReposearchComponent
   },
   {
     path: '',
