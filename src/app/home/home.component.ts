d7d815f210f262abedd4286a7a3ba244364dfb5c
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-29 08:49:28 +0300

    creating an instance of class gitubservice

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,23 @@ export class HomeComponent implements OnInit {
   githubService: HomeService;
   constructor(githubService:HomeService) {
     this.githubService = githubService
+  }
   ngOnInit(): void {
+    this.githubService.getUserDetails('JosephKinyua')
+    this.repos = this.githubService.repoData
+    this.userdetail = this.githubService.userdetails
+  }
+  maxvalue(){
+    let fun = Number.MIN_VALUE;
+    let arr = this.repos
+
+    for (let i=0; i<arr.length;i++){
+      if(arr[i].forks>fun){
+        fun = arr[i].forks
+      }
+    }
+    return fun;
   }
 
-}
+  }
+
